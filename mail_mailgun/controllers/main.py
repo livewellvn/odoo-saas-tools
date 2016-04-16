73cea7852fe9cda9f31d1272f2d97c709ccfdc1f
Ildar Nasyrov <nasyrov@it-projects.info>
2016-04-16 17:08:28 +0500

    [REF] mail_mailgun: csrf=False

--- mail_mailgun/controllers/main.py
+++ mail_mailgun/controllers/main.py
@@ -6,6 +6,6 @@ import werkzeug
 
 class MailMailgun(http.Controller):
 
-    @http.route('/mail_mailgun', auth='public')
+    @http.route('/mail_mailgun', auth='public', csrf=False)
     def incoming_mail(self, **kw):
         print '\n\n\n', 'in incoming_mail ', 'kw ', kw, '\n\n\n\n'
