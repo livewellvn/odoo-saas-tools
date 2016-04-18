99d6e8d92a3a379811f048b47975e10f6ad856e4
Ildar Nasyrov <nasyrov@it-projects.info>
2016-04-18 18:20:52 +0500

    [ADD] mail_mailgun: res.partner

--- mail_mailgun/controllers/main.py
+++ mail_mailgun/controllers/main.py
@@ -15,7 +15,7 @@ class MailMailgun(http.Controller):
     def incoming_mail_mime(self, **kw):
         print '\n\n\n', 'in incoming_mail_mime ', 'kw ', kw, '\n\n\n\n'
         body_mime = kw.get('body-mime')
-        mail_thread_obj = request.env['mail.thread']
+        mail_thread_obj = request.env['res.partner']
         msg_dict = mail_thread_obj.message_parse(body_mime)
         print '\n\n\n', 'msg_dict ', msg_dict, '\n\n\n'
         msg_id = msg_dict.get('message_id')
