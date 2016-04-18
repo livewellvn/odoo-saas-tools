75cdb8f869c04b85d00fdef3870adc25f3c995db
Ildar Nasyrov <nasyrov@it-projects.info>
2016-04-18 17:21:05 +0500

    [FIX] mail_mailgun: typo

--- mail_mailgun/controllers/main.py
+++ mail_mailgun/controllers/main.py
@@ -14,7 +14,7 @@ class MailMailgun(http.Controller):
     def incoming_mail_mime(self, **kw):
         print '\n\n\n', 'in incoming_mail_mime ', 'kw ', kw, '\n\n\n\n'
         body_mime = kw.get('body-mime')
-        mail_thread_obj = request.env['mail.theread']
+        mail_thread_obj = request.env['mail.thread']
         msg_dict = mail_thread_obj.message_parse(body_mime)
         print '\n\n\n', 'msg_dict ', msg_dict, '\n\n\n'
 
