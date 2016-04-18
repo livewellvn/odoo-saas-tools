0de637f66f3d1b6e453792705f226dcd7444ab4a
Ildar Nasyrov <nasyrov@it-projects.info>
2016-04-18 13:28:23 +0500

    [ADD] mail_mailgun: mime controller

--- mail_mailgun/controllers/main.py
+++ mail_mailgun/controllers/main.py
@@ -9,3 +9,7 @@ class MailMailgun(http.Controller):
     @http.route('/mail_mailgun', auth='public', csrf=False)
     def incoming_mail(self, **kw):
         print '\n\n\n', 'in incoming_mail ', 'kw ', kw, '\n\n\n\n'
+
+    @http.route('/mail_mailgun_mime', auth='public', csrf=False)
+    def incoming_mail_mime(self, **kw):
+        print '\n\n\n', 'in incoming_mail_mime ', 'kw ', kw, '\n\n\n\n'
