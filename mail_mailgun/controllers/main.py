32f8ed630d7038f176adcc64af35577ff0a53df5
Ildar Nasyrov <nasyrov@it-projects.info>
2016-04-20 11:20:02 +0500

    [FIX] mail_mailgun: fix event request

--- mail_mailgun/controllers/main.py
+++ mail_mailgun/controllers/main.py
@@ -10,7 +10,7 @@ class MailMailgun(http.Controller):
 
     def get_logs(self):
         return requests.get(
-        "https://api.mailgun.net/v3/YOUR_DOMAIN_NAME/events",
+        "https://api.mailgun.net/v3/iledarn.ru/events",
         auth=("api", "key-8345fd06eeb8b27a3b1f9a1e025519ad"),
         params={"begin"       : "Fri, 3 May 2013 09:00:00 -0000",
                 "ascending"   : "yes",
