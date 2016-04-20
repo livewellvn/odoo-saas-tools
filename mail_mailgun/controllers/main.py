f9b32ff73385d5a4eee68cc8afaeab0d66856516
Ildar Nasyrov <nasyrov@it-projects.info>
2016-04-20 09:55:20 +0500

    [ADD] mail_mailgun: show events

--- mail_mailgun/controllers/main.py
+++ mail_mailgun/controllers/main.py
@@ -3,13 +3,26 @@ from openerp.addons.web import http
 from openerp.addons.web.http import request
 import werkzeug
 import email
+import requests
 
 
 class MailMailgun(http.Controller):
 
+    def get_logs():
+        return requests.get(
+        "https://api.mailgun.net/v3/YOUR_DOMAIN_NAME/events",
+        auth=("api", "key-8345fd06eeb8b27a3b1f9a1e025519ad"),
+        params={
+                "ascending"   : "yes",
+                "limit"       :  25,
+                "pretty"      : "yes",
+                })
+
     @http.route('/mail_mailgun', auth='public', csrf=False)
     def incoming_mail(self, **kw):
         print '\n\n\n', 'in incoming_mail ', 'kw ', kw, '\n\n\n\n'
+        logs = self.get_logs()
+        print '\n\n\n', 'logs ', logs, '\n\n\n'
 
     @http.route('/mail_mailgun_mime', auth='public', csrf=False)
     def incoming_mail_mime(self, **kw):
