06d7c538f9e0bbaeb0732ac314022f0736a2c46d
Minh Nguyen <minh@kiuglobal.com>
2017-10-24 17:35:17 +0700

    fix translate

--- res_partner_custom2/models/res_partner.py
+++ res_partner_custom2/models/res_partner.py
@@ -11,27 +11,27 @@ class Partner(models.Model):
     tax_code = fields.Char('Tax code')
     establishment_year = fields.Datetime(string='Year of establishment')
     employee_number = fields.Integer(string='Number of employee')
-    company_size = fields.Selection(string='Company size', selection='_get_company_sizes')
+    company_size = fields.Selection(string='Company size', selection='_get_company_sizes', translate=True)
     account_currency_id = fields.Many2one('res.currency', string='Banking account currency')
     previous_year_turnover = fields.Integer(string='Previous year turn-over')
-    gender = fields.Selection(string='Gender', selection='_get_genders')
+    gender = fields.Selection(string='Gender', selection='_get_genders', translate=True)
 
     @api.model
     def _get_company_sizes(self):
         company_sizes = [
-                ('1-5', _('< 5 employees')),
-                ('5-20', _('5 - 20 employees')),
-                ('20-50', _('20 - 50 employees')),
-                ('50-250', _('20 - 250 employees')),
-                ('250-over', _('> 250 employees')),
+                ('1-5', '< 5 employees'),
+                ('5-20', '5 - 20 employees'),
+                ('20-50', '20 - 50 employees'),
+                ('50-250', '20 - 250 employees'),
+                ('250-over', '> 250 employees'),
                 ]
 
         return company_sizes
 
     def _get_genders(self):
         genders = [
-                ('male', _('Male')),
-                ('female', _('Female')),
+                ('male', 'Male'),
+                ('female', 'Female'),
                 ]
 
         return genders
