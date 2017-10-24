ff746ddc8d7e947a9a74d2c04bdccefb5c2a326e
Minh Nguyen <minh@kiuglobal.com>
2017-10-24 17:10:43 +0700

    fix translate

--- res_partner_custom2/models/res_partner.py
+++ res_partner_custom2/models/res_partner.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models
-
+# kiu
+from odoo.tools.translate import _
 
 class Partner(models.Model):
     _inherit = "res.partner"
@@ -18,19 +19,19 @@ class Partner(models.Model):
     @api.model
     def _get_company_sizes(self):
         company_sizes = [
-                ('1-5', '< 5 employees'),
-                ('5-20', '5 - 20 employees'),
-                ('20-50', '20 - 50 employees'),
-                ('50-250', '20 - 250 employees'),
-                ('250-over', '> 250 employees'),
+                ('1-5', _('< 5 employees')),
+                ('5-20', _('5 - 20 employees')),
+                ('20-50', _('20 - 50 employees')),
+                ('50-250', _('20 - 250 employees')),
+                ('250-over', _('> 250 employees')),
                 ]
 
         return company_sizes
 
     def _get_genders(self):
         genders = [
-                ('male', 'Male'),
-                ('female', 'Female'),
+                ('male', _('Male')),
+                ('female', _('Female')),
                 ]
 
         return genders
