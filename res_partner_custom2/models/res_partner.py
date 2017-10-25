db98787069c3b6329d3c0526c14b1f5c86beb2ed
Minh Nguyen <minh@kiuglobal.com>
2017-10-25 15:46:06 +0700

    fix lang

--- res_partner_custom2/models/res_partner.py
+++ res_partner_custom2/models/res_partner.py
@@ -11,27 +11,38 @@ class Partner(models.Model):
     tax_code = fields.Char('Tax code')
     establishment_year = fields.Datetime(string='Year of establishment')
     employee_number = fields.Integer(string='Number of employee')
-    company_size = fields.Selection(string='Company size', selection='_get_company_sizes', translate=True)
+    #company_size = fields.Selection(string='Company size', selection='_get_company_sizes', translate=True)
+    company_size = fields.Selection([
+        ('1-5', '< 5 employees'),
+        ('5-20', '5 - 20 employees'),
+        ('20-50', '20 - 50 employees'),
+        ('50-250', '20 - 250 employees'),
+        ('250-over', '> 250 employees')], string='Company size', translate=True)
+
     account_currency_id = fields.Many2one('res.currency', string='Banking account currency')
     previous_year_turnover = fields.Integer(string='Previous year turn-over')
-    gender = fields.Selection(string='Gender', selection='_get_genders', translate=True)
+    # gender = fields.Selection(string='Gender', selection='_get_genders', translate=True)
+    gender = fields.Selection([
+        ('male', 'Male'),
+        ('female', 'Female')
+        ], string='Gender', translate=True)
 
-    @api.model
-    def _get_company_sizes(self):
-        company_sizes = [
-                ('1-5', '< 5 employees'),
-                ('5-20', '5 - 20 employees'),
-                ('20-50', '20 - 50 employees'),
-                ('50-250', '20 - 250 employees'),
-                ('250-over', '> 250 employees'),
-                ]
+    #@api.model
+    #def _get_company_sizes(self):
+    #    company_sizes = [
+    #            ('1-5', '< 5 employees'),
+    #            ('5-20', '5 - 20 employees'),
+    #            ('20-50', '20 - 50 employees'),
+    #            ('50-250', '20 - 250 employees'),
+    #            ('250-over', '> 250 employees'),
+    #            ]
 
-        return company_sizes
+    #    return company_sizes
 
-    def _get_genders(self):
-        genders = [
-                ('male', 'Male'),
-                ('female', 'Female'),
-                ]
+    #def _get_genders(self):
+    #    genders = [
+    #            ('male', 'Male'),
+    #            ('female', 'Female'),
+    #            ]
 
-        return genders
+    #    return genders
